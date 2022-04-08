4a9a55cb050e16224135ca44729ccf352900d3df
chandan_naresh <chandansutradhar2@gmail.com>
2022-04-08 16:39:38 +0530

    setting up 2 way data binding

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -11,12 +11,18 @@ export function Login() {
 				<div
 					style={{ display: "flex", flexDirection: "column", width: "450px" }}
 				>
-					<input type="text" value={email} placeholder="Enter EmailId" />
+					<input
+						type="text"
+						value={email}
+						placeholder="Enter EmailId"
+						onChange={(ev) => setEmail(ev.target.value)}
+					/>
 
 					<input
 						type="password"
 						value={password}
 						placeholder="Enter Password"
+						onChange={(ev) => setPassword(ev.target.value)}
 					/>
 
 					<button onClick={onLogin}>Login</button>

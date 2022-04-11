3a64ee1998abd70fddd950470efdc779ee3fe963
chandan_naresh <chandansutradhar2@gmail.com>
2022-04-11 16:50:14 +0530

    setting up Context

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,9 @@ import { InvestmentView } from "./Investment/InvestmentView";
 import { MutualFund } from "./Investment/MutualFund";
 import { DebtFund } from "./Investment/DebtFund";
 import { Stocks } from "./Investment/Stocks";
+import { User } from "./models/User";
 
-export const StateContext = createContext({
+const defaultValue = {
 	expenses: [
 		{
 			amount: 300,
@@ -23,45 +24,57 @@ export const StateContext = createContext({
 			title: "edede",
 		},
 	],
-	user: null,
-});
+};
+
+//step 1: create intial state values
+const userObj: User = {
+	dob: 0,
+	email: "",
+	fullName: "",
+	country: "",
+	mobileNo: "",
+};
+//step 2: initialize context using createContext hook
+export const UserContext = createContext(userObj);
+
+export const StateContext = createContext(defaultValue);
 
 // const userContext = createContext(null);
 // const ExpenseContext = createContext(null);
 
 function App() {
-	const [appState, setAppState] = useState({
-		expenses: [
-			{
-				amount: 300,
-				category: "Travel Expenses",
-				createdBy: "chandan",
-				createdOn: 1649672825391,
-				expDate: 2000,
-				title: "edede",
-			},
-		],
-		user: null,
-	});
+	const [appState, setAppState] = useState(defaultValue);
+	const [user, setUser] = useState(userObj);
+
+	//todo: logic to identfy and find user;
+
+	//initial state update
+	let tmp = localStorage.getItem("user");
+	if (tmp) {
+		setUser(JSON.parse(tmp));
+	}
 
 	return (
-		<StateContext.Provider value={appState}>
-			<div className="App">
-				<BrowserRouter>
-					<MenuBar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/expense" element={<ExpensView />} />
-						<Route path="/investment" element={<InvestmentView />}>
-							<Route path="/investment/mf" element={<MutualFund />} />
-							<Route path="/investment/share" element={<Stocks />} />
-							<Route path="/investment/debt" element={<DebtFund />} />
-						</Route>
-					</Routes>
-				</BrowserRouter>
-			</div>
-		</StateContext.Provider>
+		//setting up UserCOntext across app by passing state user value
+		<UserContext.Provider value={user}>
+			<StateContext.Provider value={appState}>
+				<div className="App">
+					<BrowserRouter>
+						<MenuBar />
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/login" element={<Login />} />
+							<Route path="/expense" element={<ExpensView />} />
+							<Route path="/investment" element={<InvestmentView />}>
+								<Route path="/investment/mf" element={<MutualFund />} />
+								<Route path="/investment/share" element={<Stocks />} />
+								<Route path="/investment/debt" element={<DebtFund />} />
+							</Route>
+						</Routes>
+					</BrowserRouter>
+				</div>
+			</StateContext.Provider>
+		</UserContext.Provider>
 	);
 }
 
